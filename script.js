eb999e39243a5829e24f72b71d1dcc7df9ba08df
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-18 21:58:18 -0400

    create functions for the form event

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const password1El = document.getElementById('password1');
 const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
+
+let isValid = false;
+
+function validateForm() {
+    // Using Contraint API
+    isValid = form.checkValidity();
+    message.textContent = 'Please fill out all fields';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+}
+
+
+function processFormData (event) {
+    event.preventDefault();
+    validateForm();
+}
+
+
+// Event Listeners
+form.addEventListener('submit', processFormData);
